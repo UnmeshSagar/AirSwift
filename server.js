5db25b4a5e7d1d187312295acb2b357c24cdce4d
Unmesh <unmeshsagar7@gmail.com>
2023-08-03 14:30:46 +0530

    frontend

--- server.js
+++ server.js
@@ -4,6 +4,31 @@ const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
 
+const io = require("socket.io")(server);
+
+
+
 app.use(express.static(path.join(__dirname+"/public")));
+
+
+io.on("Connection", function(socket){
+    socket.on("sender-join", fuction(data){
+        socket.join(data.uid);
+    });
+    socket.on("receiver-join", fuction(data){
+        socket.join(data.uid);
+        socket.in(data.sender_uid).emit("init",data.uid);
+    });
+    socket.on("file-meta", function(data){
+        socket.in(data.uid).emit("fs-meta",data.metadata);
+    });
+    socket.on("fs-start", fuction(data){
+        socket.in(data.uid).emit("fs-share",{});
+    });
+    socket.on("file-raw", function(data){
+        socket.in(data.uid).emit("fs-share",data.buffer);
+    });
+});
+
 
 server.listen(5000);
